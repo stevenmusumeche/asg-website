fa2e60f8864e55659e22beee85949256f437a847
Davin Smith <davinsmith@outlook.com>
2019-02-16 00:23:18 -0600

    Converted to TypeScript

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,11 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from "gatsby";
+import React from "react";
 
-const Header = ({ siteTitle }) => (
+interface IHeaderProps {
+  siteTitle: string;
+}
+
+const Header = ({ siteTitle = "" }: IHeaderProps) => (
   <header
     style={{
       background: `rebeccapurple`,
@@ -29,14 +32,6 @@ const Header = ({ siteTitle }) => (
       </h1>
     </div>
   </header>
-)
+);
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+export default Header;

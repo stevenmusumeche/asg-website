b1e702ecd9d76c306a9d9d715a3740f7d319822f
Steven Musumeche <steven.musumeche@external.milliporesigma.com>
2019-02-23 14:48:42 -0600

    change data fetching for meetup.com to use their API

--- backend/services/calendar.ts
+++ backend/services/calendar.ts
@@ -1,31 +1,15 @@
 import axios from "axios";
-import { parse as parseXml } from "fast-xml-parser";
-import * as ICAL from "ical.js";
+import * as h2p from "html2plaintext";
 import {
   GoogleCalendarItems,
   GoogleCalendarItem,
-} from "../types/googleCalendar";
-import { parse } from "url";
+  Event,
+  EventSource,
+  EventFetcher,
+  MeetupEvent,
+} from "../types/events";
 
-export interface Event {
-  id: string;
-  name: string;
-  location: string;
-  url?: string;
-  startDate: Date;
-  endDate: Date;
-  source: string;
-  description?: string;
-}
-
-type Fetcher = (name: string) => Promise<Event[]>;
-
-interface EventSource {
-  name: string;
-  fetcher: Fetcher;
-}
-
-// todo: cache results
+// todo: cache results: https://www.npmjs.com/package/serverless-api-gateway-caching
 
 /**
  * Places where we get events from
@@ -40,7 +24,7 @@ const eventSources: EventSource[] = [
   },
   {
     name: "AWS Lafayette",
-    fetcher: fetchFromICal("https://www.meetup.com/AWS-Lafayette/events/ical/"),
+    fetcher: fetchFromMeetup("AWS-Lafayette"),
   },
 ];
 
@@ -64,40 +48,40 @@ export async function listEvents(): Promise<Event[]> {
     .sort(sortByStartDate);
 }
 
-function fetchFromICal(url: string): Fetcher {
+function fetchFromMeetup(meetupUrlName) {
   return async function fetcher(source: string) {
-    const result = await axios.get(url);
+    const url = `https://api.meetup.com/${meetupUrlName}/events?photo-host=public&page=200&key=${
+      process.env.MEETUP_API_KEY
+    }`;
+    const { data }: { data: MeetupEvent[] } = await axios.get(url);
 
-    const jcalData = ICAL.parse(result.data);
-    const component = new ICAL.Component(jcalData);
-    const vEvents = component.getAllSubcomponents("vevent");
+    return data.map(mapMeetupToEvent).map(event => ({ ...event, source }));
+  };
+}
 
-    return vEvents.map(vEvent => {
-      const event = new ICAL.Event(vEvent);
-
-      // url format is https://www.meetup.com/AWS-Lafayette/events/[EVENTID]/
-      // event ID format is event_[EVENTID]@meetup.com
-      const matches = event.uid.match(/event_(.*?)@meetup\.com/i);
-      const eventId = matches[1];
-
-      return {
-        id: eventId,
-        name: event.summary,
-        description: event.description,
-        startDate: event.startDate.toJSDate(),
-        endDate: event.endDate.toJSDate(),
-        location: event.location,
-        url: `https://www.meetup.com/AWS-Lafayette/events/${eventId}/`,
-        source,
-      } as Event;
-    });
+function mapMeetupToEvent(event: MeetupEvent): Event {
+  return {
+    id: event.id,
+    name: event.name,
+    location: `${event.venue.address_1}${event.venue.address_2 &&
+      " " + event.venue.address_2}, ${event.venue.city}, ${event.venue.state} ${
+      event.venue.zip
+    }`,
+    url: event.link,
+    startDate: new Date(event.time),
+    endDate: new Date(event.time + event.duration),
+    source: event.group.name,
+    description: h2p(event.description),
   };
 }
 
 /**
  * Fetch all events from a specific google calendar
  */
-function fetchFromGoogleCalendar(calendarId: string, apiKey: string): Fetcher {
+function fetchFromGoogleCalendar(
+  calendarId: string,
+  apiKey: string
+): EventFetcher {
   return async function fetcher(source: string) {
     try {
       const calendarUrl = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${apiKey}`;
@@ -127,44 +111,7 @@ function mapGoogleToEvent(event: GoogleCalendarItem): Event {
 }
 
 function sortByStartDate(a: Event, b: Event) {
-  if (a.startDate > b.startDate) return -1;
-  if (a.startDate < b.startDate) return 1;
+  if (a.startDate > b.startDate) return 1;
+  if (a.startDate < b.startDate) return -1;
   return 0;
-
-  //return b.startDate - a.startDate;
 }
-
-const foo = [
-  "vevent",
-  [
-    ["dtstamp", {}, "date-time", "2019-02-21T23:34:42Z"],
-    ["dtstart", [Object], "date-time", "2019-02-21T18:00:00"],
-    ["dtend", [Object], "date-time", "2019-02-21T20:00:00"],
-    ["status", {}, "text", "CONFIRMED"],
-    ["summary", {}, "text", "CI/CD Pipelines"],
-    [
-      "description",
-      {},
-      "text",
-      "AWS Lafayette\nThursday, February 21 at 6:00 PM\n\nPresentation on CI/CD using AWS CodeSuite tools followed by an open discussion about this topic. Join us in learning about the latest in cloud technol...\n\nhttps://www.meetup.com/AWS-Lafayette/events/257276074/",
-    ],
-    ["class", {}, "text", "PUBLIC"],
-    ["created", {}, "date-time", "2018-12-17T03:56:22Z"],
-    ["geo", {}, "float", [Array]],
-    [
-      "location",
-      {},
-      "text",
-      "214 Jefferson St (214 Jefferson St, Lafayette, LA 70501)",
-    ],
-    [
-      "url",
-      {},
-      "uri",
-      "https://www.meetup.com/AWS-Lafayette/events/257276074/",
-    ],
-    ["last-modified", {}, "date-time", "2019-02-21T11:52:42Z"],
-    ["uid", {}, "text", "event_257276074@meetup.com"],
-  ],
-  [],
-];
---
+++ backend/types/events.ts
@@ -0,0 +1,73 @@
+export interface Event {
+  id: string;
+  name: string;
+  location: string;
+  url?: string;
+  startDate: Date;
+  endDate: Date;
+  source: string;
+  description?: string;
+}
+
+export interface GoogleCalendarItem {
+  kind: "calendar#event";
+  id: string;
+  status: string;
+  htmlLink: string;
+  updated: string;
+  summary: string;
+  location: string;
+  creator: { email: string; displayName: string };
+  organizer: {
+    email: string;
+    displayName: string;
+    self: boolean;
+  };
+  start: { dateTime: string };
+  end: { dateTime: string };
+  transparency: string;
+  iCalUID: string;
+  sequence: number;
+  extendedProperties: { private: { everyoneDeclinedDismissed: string } };
+  guestsCanSeeOtherGuests: boolean;
+}
+
+export interface GoogleCalendarItems {
+  items: GoogleCalendarItem[];
+}
+
+export type EventFetcher = (name: string) => Promise<Event[]>;
+
+export interface EventSource {
+  name: string;
+  fetcher: EventFetcher;
+}
+
+export interface MeetupEvent {
+  id: string;
+  name: string;
+  time: string;
+  duration: number;
+  local_date: string;
+  local_time: string;
+  utc_offset: number;
+  yes_rsvp_count: number;
+  venue: {
+    id: number;
+    name: string;
+    lat: number;
+    long: number;
+    address_1: string;
+    address_2: string;
+    city: string;
+    country: string;
+    zip: string;
+    state: string;
+  };
+  group: {
+    id: number;
+    name: string;
+  };
+  link: string;
+  description: string;
+}

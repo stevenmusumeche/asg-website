bec844975f61e2d3ebe2386ac7c0a16220ef6d56
Steven Musumeche <steven@musumeche.com>
2022-09-24 15:02:59 -0500

    fix build

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,13 +42,6 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
-    // `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-plugin-favicon`,
-      options: {
-        logo: "./src/images/favicon.png",
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
